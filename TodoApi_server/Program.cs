0ba6429347ab4653a0f222ab0660f7a1c5e352c2
RivkaSchonfeld <rivkashon5@gmail.com>
2025-03-20 20:55:50 +0200

    connection strings

--- TodoApi_server/Program.cs
+++ TodoApi_server/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ToDoDbContext>(options =>
-    options.UseMySql("server=localhost;user=root;password=1234;database=ToDoDB",
+    options.UseMySql("server=bzihwsot5wnlw05shmep-mysql.services.clever-cloud.com;user=uc1h6zquvck9wpdk;password=W9LIbD5yyy1T36bm39BU;database=bzihwsot5wnlw05shmep",
      Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.41-mysql")));
 
 var app = builder.Build();
